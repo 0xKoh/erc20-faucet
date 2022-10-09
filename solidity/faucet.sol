ec3aa0a45dc4b4d5dca8d93d6ff4ab1c408b83de
KouGa <kouga08082361184@gmail.com>
2022-10-10 02:13:00 +0900

    FaucetにvariableSend関数とconstantSend関数とsetLimit関数を追加。

--- solidity/faucet.sol
+++ solidity/faucet.sol
@@ -13,41 +13,55 @@ interface ERC20 {
  
 }
 
-contract mintDapps {
+contract Faucet {
     
     ERC20 token;
     
     address owner;
+
+    uint256 value;
+
+    uint256 limit;
+
     
-    uint256 faucetAmount = 10;
-    
-    constructor (address _tokenAddress, address _ownerAddress) {
+    constructor (address _tokenAddress, address _ownerAddress, uint256 _value, uint256 _limit) {
         token = ERC20(_tokenAddress);
         owner = _ownerAddress;
+        value = _value;
+        limit = _limit;
     }   
     
     modifier onlyOwner{
-        require(msg.sender == owner,"FaucetError: Caller not owner");
+        require(msg.sender == owner, "Error: It is not the address of the administrator.");
         _;
-    }    
-
-    function mintToken(uint256 _amount) payable external {
-        require(token.balanceOf(address(this)) > _amount, "FaucetError: ");
-        require(msg.value == 100 / 10**token.decimals(), "FaucetError: ");
+    }
+    
+    function variableSend(uint256 _amount) external {
+        require(token.balanceOf(address(this)) >= _amount, "Error: There is no balance in Faucet.");
+        require(limit >= _amount, "Error: The maximum amount that can be withdrawn has been exceeded.");
         
         token.transfer(msg.sender, _amount * 10**token.decimals());
-    }  
+    }
+        
+    function constantSend() external {
+        require(token.balanceOf(address(this)) >= value, "Error: There is no balance in Faucet.");
+        token.transfer(msg.sender, value * 10**token.decimals());
+    }
 
     function setTokenAddress(address _addToken) external onlyOwner {
         token = ERC20(_addToken);
     }    
 
     function setFaucetAmount(uint256 _amount) external onlyOwner {
-        faucetAmount = _amount;
+        value = _amount;
+    }
+
+    function setLimit(uint256 _amount) external onlyOwner {
+        limit = _amount;
     }  
      
     function withdrawTokens(address _receiver, uint256 _amount) external onlyOwner {
-        require(token.balanceOf(address(this)) >= _amount,"FaucetError: Insufficient funds");
+        require(token.balanceOf(address(this)) >= _amount, "Error: There is no balance in Faucet.");
         token.transfer(_receiver,_amount);
     }
 }
